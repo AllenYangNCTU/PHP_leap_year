c6cc45084bc75a88f06d22220a04035140470837
Yanghejie <yanghejie.bt04@nycu.edu.tw>
2022-04-18 22:02:05 +0800

    second commit

--- leapyear.php
+++ leapyear.php
@@ -1,13 +1,6 @@
 <?php
-$year = 2019;
-if ($year % 400 == 0) {
-   echo $year." is a leap year.";
-} elseif ($year % 100 == 0) {
-   echo $year." is not a leap year.";
-} elseif ($year % 4 == 0) {
-   echo $year." is a leap year.";
-} else {
-   echo $year." is not a leap year.";
-}
+$year = 2022;
+if (($year % 400 == 0) || ($year % 100 != 0 && $year % 4 == 0)) 
+     print($year." is a leap year");  
+else print($year." is a common year");
 ?>
-<h1>hello</h1>
